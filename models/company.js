ced16439dd5e62a5767d0086b0517569b26e2ccd
mar69287 <marcoa.ruiz97@gmail.com>
2023-03-07 19:51:43 -0800

    company model

---
+++ models/company.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const employeeSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    hireDate: {
+        type: Date,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    position: String
+}, {
+    timestamps: true,
+});
+
+const companySchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    employees: [employeeSchema],
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    revenue: {
+        type: Number
+    },
+    active: {
+        type: Boolean,
+        required: true
+    },
+}, {
+    timestamps: true,
+});
+
+module.exports = mongoose.model('Company', companySchema);
