bf09d2ee27cf135eb3abc9d39712ccf39d2e85b6
mar69287 <marcoa.ruiz97@gmail.com>
2023-03-09 13:56:48 -0800

    revenue bar graph

---
+++ src/components/SalesBarChart /SalesBarChart.jsx
@@ -0,0 +1,94 @@
+import { ResponsiveBar } from '@nivo/bar'
+import { useState, useEffect } from 'react';
+import { getCompanies } from '../../utilities/companies-api';
+
+export default function SalesBarChart() {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        async function fetchSalesData() {
+            const companies = await getCompanies();
+            const salesData = companies.map(company => ({
+                id: company.name,
+                Sales: company.sales
+            }));
+            setData(salesData);
+        }
+        fetchSalesData();
+    }, []);
+
+    const lineGraphSettings = {
+        theme: {
+            fontSize: '.9rem',
+            textColor: '#7d8a99',
+        },
+    };
+
+    return (
+        <ResponsiveBar
+            data={data}
+            keys={['Sales']}
+            indexBy="id"
+            margin={{ top: 30, right: 50, bottom: 130, left: 70 }}
+            padding={0.5}
+            valueScale={{ type: 'linear' }}
+            indexScale={{ type: 'band', round: true }}
+            colors={{ scheme: 'set2' }}
+            theme={lineGraphSettings.theme}
+            defs={[
+                {
+                    id: 'dots',
+                    type: 'patternDots',
+                    background: 'inherit',
+                    color: '#38bcb2',
+                    size: 4,
+                    padding: 1,
+                    stagger: true
+                },
+                {
+                    id: 'lines',
+                    type: 'patternLines',
+                    background: 'inherit',
+                    color: '#eed312',
+                    rotation: -45,
+                    lineWidth: 6,
+                    spacing: 10
+                }
+            ]}
+            borderColor={{ theme: 'background' }}
+            axisTop={null}
+            axisRight={null}
+            axisBottom={{
+                tickSize: 5,
+                tickPadding: 10,
+                tickRotation: -45,
+                tickTextColor: '#333333'
+            }}
+            axisLeft={{
+                tickSize: 6,
+                tickPadding: 10,
+                tickRotation: 0,
+                tickTextColor: '#333333',
+                legendPosition: 'middle',
+
+                legendTextColor: '#333333'
+            }}
+            enableLabel={false}
+            labelSkipWidth={12}
+            labelSkipHeight={12}
+            labelTextColor={{
+                from: 'color',
+                modifiers: [
+                    [
+                        'darker',
+                        '1'
+                    ]
+                ]
+            }}
+
+            role="application"
+            ariaLabel="Nivo bar chart demo"
+            barAriaLabel={function (e) { return e.id + ": " + e.formattedValue + " in country: " + e.indexValue }}
+        />
+    )
+}
--- src/pages/DashboardPage/DashboardPage.jsx
+++ src/pages/DashboardPage/DashboardPage.jsx
@@ -1,15 +1,16 @@
 import { Link } from 'react-router-dom';
+import RevenueBarChart from '../../components/RevenueBarChart/RevenueBarChart'
+// import SalesBarChart from '../../components/SalesBarChart/SalesBarChart'
 import './DashboardPage.css';
 
 export default function DashboardPage() {
+
     return (
         <section className='dashboard-home'>
-            {/* <div className="title">
-                <h1>Insightful Business <span>Management</span> </h1>
-                <h1>At Your Fingertips</h1>
+            <div className="graph-container">
+                <h1>Revenue Report</h1>
+                <RevenueBarChart />
             </div>
-            <Link to="/signup" className="signup">Sign Up</Link> */}
-            <h1>Dashboard</h1>
         </section>
     )
 }
