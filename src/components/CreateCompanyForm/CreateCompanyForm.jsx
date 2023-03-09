b4749e97a30315bfb4bbfe36b38893ba561a2050
mar69287 <marcoa.ruiz97@gmail.com>
2023-03-08 17:35:32 -0800

    css to delete button

--- src/components/CreateCompanyForm/CreateCompanyForm.jsx
+++ src/components/CreateCompanyForm/CreateCompanyForm.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { createCompany } from '../../utilities/companies-api'
+import { useNavigate } from "react-router-dom";
 
 export default function CreateCompanyForm({ user, setCompanies }) {
   const [newCompany, setNewCompany] = useState({
@@ -7,6 +8,7 @@ export default function CreateCompanyForm({ user, setCompanies }) {
     revenue: 0,
     active: false,
   })
+  const navigate = useNavigate()
 
   const handleAddCompany = async (evt) => {
     evt.preventDefault();
@@ -23,6 +25,7 @@ export default function CreateCompanyForm({ user, setCompanies }) {
         revenue: 0,
         active: false,
       });
+      navigate('/companies')
     } catch (err) {
       console.log(err);
     }
